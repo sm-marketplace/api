ef6a2183962cec680a4bf5e6d64bebf9ee74cb46
Roger Ramos <roger.ramos@gdpteam.com>
2022-12-19 17:28:42 -0500

    feat: rate limiter to search endpoint

--- src/app.js
+++ src/app.js
@@ -18,6 +18,7 @@ app.use(fileUpload({ createParentPath: true })); // Enable files upload
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/upload-file', rateLimiter) // CodeQL: js/missing-rate-limiting
+app.use('/search', rateLimiter) // CodeQL: js/missing-rate-limiting
 
 // Routes
 app.post('/upload-file', handlePostItemPinata);
